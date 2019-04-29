3237a3f09f29160916e97430852c2a97ce01f318
Nathan Cruz <nathan_cruz77@protonmail.com>
2019-04-29 17:57:47 -0300

    Add generic multi-select component with autocomplete

---
+++ frontend/components/multi-select.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-autocomplete
+    v-model="selected"
+    :disabled="isUpdating"
+    :items="options"
+    :label="label"
+    :allow-overflow="false"
+    hide-selected
+    hide-no-data
+    clearable
+    chips
+    color="blue-grey lighten-2"
+    multiple
+    no-data-text="Carregando">
+  </v-autocomplete>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    selected: [],
+    autoUpdate: true,
+    isUpdating: false,
+  }),
+
+  watch: {
+    isUpdating (val) {
+      if (val) {
+        setTimeout(() => (this.isUpdating = false), 3000)
+      }
+    }
+  },
+}
+</script>
