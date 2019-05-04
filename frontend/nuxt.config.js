fb87bd6ac36cefa21feb76da133451437ce35af1
Nathan Cruz <nathan_cruz77@protonmail.com>
2019-05-04 12:36:56 -0300

    Change favicon and meta description tag

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Foodnator - Find the best restaurant for your group' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
