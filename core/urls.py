91d315867c483e537b9ebabce1151709aa1dbe0d
Nathan Cruz <nathan_cruz77@protonmail.com>
2019-05-05 00:10:38 -0300

    Remove unused code from example app (todo list)

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     url(r'^api/login$', views.login),
     url(r'^api/logout$', views.logout),
     url(r'^api/whoami$', views.whoami),
-
-    url(r'^api/add_todo$', views.add_todo),
-    url(r'^api/list_todos$', views.list_todos),
 ]
