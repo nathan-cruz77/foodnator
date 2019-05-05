5ddbab775a9ca08342c22ed56e6de9e08ce754d3
Nathan Cruz <nathan_cruz77@protonmail.com>
2019-05-05 19:56:27 -0300

    Recreated database...

--- core/models.py
+++ core/models.py
@@ -23,6 +23,18 @@ class ActivityLog(models.Model):
 class PriceRange(models.Model):
     name = models.CharField(max_length=64)
 
+    @classmethod
+    def from_int(cls, value):
+        mapping = {
+            1: 'CHEAPEST',
+            2: 'MODERATE',
+            3: 'CHEAP',
+            4: 'EXPENSIVE',
+            5: 'MOST_EXPENSIVE',
+        }
+
+        return cls.objects.get(name=mapping[value])
+
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=255)
@@ -43,7 +55,7 @@ class Preference(models.Model):
     only_free_delivery = models.BooleanField()
     min_rating = models.DecimalField(max_digits=2, decimal_places=1)
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     price_range = models.ForeignKey(PriceRange)
     selected_cuisines = models.ManyToManyField(Cuisine, related_name='selected_by')
     rejected_cuisines = models.ManyToManyField(Cuisine, related_name='rejected_by')
