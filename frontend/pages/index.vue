62eaed9ad9cfc9f43c205d41c6277f39dfd12007
Nathan Cruz <nathan_cruz77@protonmail.com>
2019-05-07 13:27:42 -0300

    Index page for anonymous users

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <v-layout column justify-center align-center>
+  <v-layout v-if="loggedUser" column justify-center align-center>
     <div class="display-2">Can't choose where to eat?</div>
     <div class="display-2">We got you covered!</div>
     <find-restaurant class="find-restaurant-button"/>
+  </v-layout>
+  <v-layout v-else column justify-center align-center >
+    <div class="display-2">Find the best restaurants for your group or you!</div>
+    <div class="display-2">Login to get started</div>
   </v-layout>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import AppApi from '~apijs'
 import FindRestaurant from '~/components/find-restaurant'
 
 export default {
   components: {
     FindRestaurant,
+  },
+
+  computed: {
+    ...mapState('user', ['loggedUser']),
   },
 }
 </script>
